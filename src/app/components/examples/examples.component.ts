b5e58468a7a8a3d8b1f20e51436543b1a7db3e1c
Vlad Pascu <vlad_pascu25@yahoo.com>
2022-04-17 18:51:38 +0300

    create docs page

--- src/app/components/examples/examples.component.ts
+++ src/app/components/examples/examples.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-examples',
   templateUrl: './examples.component.html',
   styleUrls: ['./examples.component.scss']
 })
-export class ExamplesComponent implements OnInit {
+export class ExamplesComponent {
   cards = [1, 2, 3];
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  masonryImages = new Array(15);
 }
